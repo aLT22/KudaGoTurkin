3652d7f3bf2ec95a9c90ce4a030e5717a4fb52b8
Alexey Turkin <altsworks@gmail.com>
2019-03-18 11:18:11 +0300

    ui: implementing city picker logic

--- app/src/main/java/com/bytebuilding/kudagoturkin/ui/main/MainActivity.kt
+++ app/src/main/java/com/bytebuilding/kudagoturkin/ui/main/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.bytebuilding.kudagoturkin.ui.main
 
+import android.os.Bundle
+import androidx.appcompat.widget.AppCompatTextView
 import com.bytebuilding.kudagoturkin.R
 import com.bytebuilding.kudagoturkin.databinding.ActivityMainBinding
 import com.bytebuilding.kudagoturkin.ui.base.BaseActivity
+import com.bytebuilding.kudagoturkin.utils.getDefaultCity
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainActivityVM>(MainActivityVM::class) {
+
+    private var mCityName: AppCompatTextView? = null
 
     override fun layoutId(): Int = R.layout.activity_main
 
     override fun initViews() {
+        mCityName = findViewById(R.id.cityName)
     }
 
     override fun initListeners() {
     }
 
     override fun removeListeners() {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mCityName?.let { view ->
+            view.text = getDefaultCity()
+        }
     }
 
     companion object {
