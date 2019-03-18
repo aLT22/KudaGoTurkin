cc1dccd6dbc420c56a66fe460dd74bfd018b348d
Alexey Turkin <altsworks@gmail.com>
2019-03-18 14:56:58 +0300

    ui: open and close ChooseCityActivity screen logic implementation

--- app/src/main/java/com/bytebuilding/kudagoturkin/ui/cities/ChooseCityActivity.kt
+++ app/src/main/java/com/bytebuilding/kudagoturkin/ui/cities/ChooseCityActivity.kt
@@ -1,21 +1,29 @@
 package com.bytebuilding.kudagoturkin.ui.cities
 
-import androidx.databinding.ViewDataBinding
+import androidx.appcompat.widget.AppCompatImageView
 import com.bytebuilding.kudagoturkin.R
+import com.bytebuilding.kudagoturkin.databinding.ActivityChooseCityBinding
 import com.bytebuilding.kudagoturkin.ui.base.BaseActivity
 
 
-class ChooseCityActivity : BaseActivity<ViewDataBinding, ChooseCityActivityVM>(ChooseCityActivityVM::class) {
+class ChooseCityActivity : BaseActivity<ActivityChooseCityBinding, ChooseCityActivityVM>(ChooseCityActivityVM::class) {
+
+    private var mClose: AppCompatImageView? = null
 
     override fun layoutId(): Int = R.layout.activity_choose_city
 
     override fun initViews() {
+        mClose = findViewById(R.id.close)
     }
 
     override fun initListeners() {
+        mClose?.setOnClickListener {
+            finish()
+        }
     }
 
     override fun removeListeners() {
+        mClose?.setOnClickListener(null)
     }
 
     companion object {
--- app/src/main/java/com/bytebuilding/kudagoturkin/ui/main/MainActivity.kt
+++ app/src/main/java/com/bytebuilding/kudagoturkin/ui/main/MainActivity.kt
@@ -2,32 +2,41 @@ package com.bytebuilding.kudagoturkin.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.LinearLayoutCompat
 import com.bytebuilding.kudagoturkin.R
 import com.bytebuilding.kudagoturkin.databinding.ActivityMainBinding
 import com.bytebuilding.kudagoturkin.ui.base.BaseActivity
-import com.bytebuilding.kudagoturkin.utils.getDefaultCity
+import com.bytebuilding.kudagoturkin.ui.cities.ChooseCityActivity
+import com.bytebuilding.kudagoturkin.utils.getDefaultCityName
+import com.bytebuilding.kudagoturkin.utils.launchActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainActivityVM>(MainActivityVM::class) {
 
+    private var mCityPickerContainer: LinearLayoutCompat? = null
     private var mCityName: AppCompatTextView? = null
 
     override fun layoutId(): Int = R.layout.activity_main
 
     override fun initViews() {
         mCityName = findViewById(R.id.cityName)
+        mCityPickerContainer = findViewById(R.id.cityPickerContainer)
     }
 
     override fun initListeners() {
+        mCityPickerContainer?.setOnClickListener { view ->
+            launchActivity<ChooseCityActivity> { }
+        }
     }
 
     override fun removeListeners() {
+        mCityPickerContainer?.setOnClickListener(null)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mCityName?.let { view ->
-            view.text = getDefaultCity()
+            view.text = getDefaultCityName()
         }
     }
 
