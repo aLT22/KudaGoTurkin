e6077518384c9bc2c2beeec7c6e4b546069f22d6
Alexey Turkin <altsworks@gmail.com>
2019-03-18 16:32:56 +0300

    dev: implementing logic for ChooseCityActivity

--- app/src/main/java/com/bytebuilding/kudagoturkin/di/Modules.kt
+++ app/src/main/java/com/bytebuilding/kudagoturkin/di/Modules.kt
@@ -2,6 +2,8 @@ package com.bytebuilding.kudagoturkin.di
 
 import com.bytebuilding.kudagoturkin.ui.cities.ChooseCityActivityVM
 import com.bytebuilding.kudagoturkin.ui.main.MainActivityVM
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
@@ -12,6 +14,6 @@ val viewModelModule = module {
      * Activity VMs
      * */
     viewModel { MainActivityVM() }
-    viewModel { ChooseCityActivityVM() }
+    viewModel { ChooseCityActivityVM(androidApplication()) }
 
 }
