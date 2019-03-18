f580af7d8de4eb7cb4f7ffcd50dea0666158a9ec
Alexey Turkin <altsworks@gmail.com>
2019-03-18 11:01:49 +0300

    model: added City model

--- app/src/main/java/com/bytebuilding/kudagoturkin/data/model/City.kt
+++ app/src/main/java/com/bytebuilding/kudagoturkin/data/model/City.kt
@@ -1,7 +1,11 @@
 package com.bytebuilding.kudagoturkin.data.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+
+@Parcelize
 data class City(
         val id: Long = -1L,
         val name: String = ""
-)
+) : Parcelable
