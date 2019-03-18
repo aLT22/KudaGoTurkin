a7a78c93ed93fdf5b7cac627c0bc5221eb270737
Alexey Turkin <altsworks@gmail.com>
2019-03-18 14:55:51 +0300

    di: added view model for choose city screen in koin module

--- app/src/main/java/com/bytebuilding/kudagoturkin/di/Modules.kt
+++ app/src/main/java/com/bytebuilding/kudagoturkin/di/Modules.kt
@@ -1,5 +1,6 @@
 package com.bytebuilding.kudagoturkin.di
 
+import com.bytebuilding.kudagoturkin.ui.cities.ChooseCityActivityVM
 import com.bytebuilding.kudagoturkin.ui.main.MainActivityVM
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
@@ -11,5 +12,6 @@ val viewModelModule = module {
      * Activity VMs
      * */
     viewModel { MainActivityVM() }
+    viewModel { ChooseCityActivityVM() }
 
 }
